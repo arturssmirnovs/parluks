85012eeafffe15d1878fb02c55d96862e4b171d4
Arturs Smirnovs <arturs@Arturss-MacBook-Air.local>
2021-01-08 18:23:05 +0200

    Mac compiled

--- compiler/mac/buider.js
+++ compiler/mac/buider.js
@@ -1,10 +1,10 @@
 var createDMG = require('electron-installer-dmg')
 createDMG({
     appPath: "../../install/mac/source/Parluks-darwin-x64/Parluks.app",
-    out: "../../install/mac/Parluks.dmg",
+    out: "../../install/mac/",
     title: "Parluks",
     name: "Parluks",
-    icon: "../../parluks/icon_32.icns",
+    icon: "../../src/icon_32.icns",
     background: "mac_bg.png",
     overwrite: true
 }, function done (err) {
